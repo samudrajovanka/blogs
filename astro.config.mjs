5c21c9b63480486f47cbe93ae717fcdb497d8ce4
Jovanka <samudrajovanka20@gmail.com>
2024-10-04 14:10:55 +0700

    fix: change adapater

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,14 @@ import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 
-import vercel from '@astrojs/vercel/serverless';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
   output: 'server',
-  adapter: vercel()
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
