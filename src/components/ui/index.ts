cc16f139a3db35718bcb40bfc79c1959ffe2d4cd
Jovanka <samudrajovanka20@gmail.com>
2024-10-04 15:29:36 +0700

    feat: add back button

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,3 +3,4 @@ export { default as Navbar } from './Navbar/index.astro';
 export { default as BlogCard } from './BlogCard/index.astro';
 export { default as BlogTime } from './BlogTime/index.astro';
 export { default as Tag } from './Tag/index.astro';
+export { default as Back } from './Back/index.astro';
