b695a4ca4c6c8905979ca0b945770db0179a3d23
Jovanka <samudrajovanka20@gmail.com>
2024-10-07 13:57:34 +0700

    fix: eslint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
 
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel()
 });
--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,3 @@
-import tsParser from '@typescript-eslint/parser';
-import astroParser from 'astro-eslint-parser';
 import eslintPluginAstro from 'eslint-plugin-astro';
 import { default as pluginImport } from 'eslint-plugin-import';
 import importAlias from 'eslint-plugin-import-alias';
@@ -13,86 +11,89 @@ export default [
       'import-alias': importAlias,
       'import': pluginImport
     },
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        sourceType: 'module',
-        ecmaVersion: 'latest'
-      }
-    },
-  },
-  {
-    files: ['*.astro'],
-    languageOptions: {
-      parser: astroParser,
-      parserOptions: {
-        parser: tsParser,
-        extraFileExtensions: ['.astro']
-      }
-    },
-    rules: {
-      quotes: [
-        'error',
-        'single',
-        {
-          'allowTemplateLiterals': true
-        }
-      ],
-      'jsx-quotes': [
-        'error',
-        'prefer-double'
-      ],
-      semi: [
-        'error',
-        'always'
-      ],
-      'no-unused-vars': [
-        'error',
-        {
-          'argsIgnorePattern': '^_',
-          'varsIgnorePattern': '^_'
-        }
-      ],
-      'no-var': [
-        'warn'
-      ],
-      'max-len': [
-        'warn',
-        120
-      ],
-      'comma-dangle': [
-        'error',
-        'never'
-      ],
-      'import/no-unresolved': 'off',
-      'import/extensions': 'off',
-      'import/no-extraneous-dependencies': 'off',
-      'import/order': [
-        'error',
-        {
-          'alphabetize': {
-            'caseInsensitive': true,
-            'order': 'asc'
-          },
-          'groups': ['builtin', 'external', 'internal'],
-          'newlines-between': 'always',
-          'pathGroups': [
-            {
-              'pattern': '@/**',
-              'group': 'external',
-              'position': 'after'
-            }
-          ]
-        }
-      ],
-      'import-alias/import-alias': [
-        'error',
-        {
-          'relativeDepth': 0
-        }
-      ],
-      'import/no-named-default': 'off',
-      'import/prefer-default-export': 'off'
-    }
+    // rules: {
+    //   quotes: [
+    //     'error',
+    //     'single',
+    //     {
+    //       'allowTemplateLiterals': true
+    //     }
+    //   ],
+    //   'jsx-quotes': [
+    //     'error',
+    //     'prefer-double'
+    //   ],
+    //   semi: [
+    //     'error',
+    //     'always'
+    //   ],
+    //   'no-unused-vars': [
+    //     'error',
+    //     {
+    //       'argsIgnorePattern': '^_',
+    //       'varsIgnorePattern': '^_'
+    //     }
+    //   ],
+    //   'no-var': [
+    //     'warn'
+    //   ],
+    //   'max-len': [
+    //     'warn',
+    //     120
+    //   ],
+    //   'comma-dangle': [
+    //     'error',
+    //     'never'
+    //   ],
+    //   'import/no-unresolved': 'off',
+    //   'import/extensions': 'off',
+    //   'import/no-extraneous-dependencies': 'off',
+    //   'import/order': [
+    //     'error',
+    //     {
+    //       'alphabetize': {
+    //         'caseInsensitive': true,
+    //         'order': 'asc'
+    //       },
+    //       'groups': ['builtin', 'external', 'internal'],
+    //       'newlines-between': 'always',
+    //       'pathGroups': [
+    //         {
+    //           'pattern': '@/**',
+    //           'group': 'external',
+    //           'position': 'after'
+    //         }
+    //       ]
+    //     }
+    //   ],
+    //   'import-alias/import-alias': [
+    //     'error',
+    //     {
+    //       'relativeDepth': 0
+    //     }
+    //   ],
+    //   'import/no-named-default': 'off',
+    //   'import/prefer-default-export': 'off'
+    // },
+    ignores: [
+      'dist/**/*',
+      '.astro/**/*',
+      'node_modules/',
+      'npm-debug.log*',
+      'yarn-debug.log*',
+      'yarn-error.log*',
+      'pnpm-debug.log*',
+      '.env',
+      '.env.production',
+      '.DS_Store',
+      'out',
+      '.vscode/settings.json',
+      '.idea/*',
+      'yarn.lock',
+      'public',
+      '.eslintcache',
+      'CHANGELOG.md',
+      '.vercel/**/*'
+    ]
   }
 ];
